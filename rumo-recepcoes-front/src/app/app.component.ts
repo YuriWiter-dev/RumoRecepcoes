da67f8f71f4b2f2d5dc10603a35dd53f29ec067f
Yuri Witer <yuriwiterconsulting@gmail.com>
2024-02-29 02:54:47 -0300

    Criação prototipo menu

--- rumo-recepcoes-front/src/app/app.component.ts
+++ rumo-recepcoes-front/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Navbar/navbar.component';
+import { AppRoutingModule } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavbarComponent],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, AppRoutingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- rumo-recepcoes-front/src/app/app.routes.ts
+++ rumo-recepcoes-front/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CozinhaComponent } from './cozinha/cozinha.component';
+import { GarcomComponent } from './garcom/garcom.component';
+import { NavbarComponent } from './Navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+// Declare e exporte a variável routes
+export const routes: Routes = [
+  { path: '',  component: NavbarComponent }, 
+  { path: 'cozinha', component: CozinhaComponent },
+  { path: '/garcom', component: GarcomComponent },
+];
 
-export const routes: Routes = [];
+@NgModule({
+  imports: [ CommonModule,
+    RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule  { }
